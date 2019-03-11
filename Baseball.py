5f7eb66c1e06b09622b19dba6a50a21f29a87a48
Logan Pennock <02logan.pennock@gulllakecs.org>
2019-03-11 12:55:33 -0400

    add variables

---
+++ Baseball.py
@@ -0,0 +1,17 @@
+# Final project
+# single, double, triple, HOMERUN!!!
+# Logan Pennock
+
+x = y
+a = b
+c = d
+for i in range(3):
+    speed_of_pitch = int(input("What is the speed of the pitch: "))
+    angle_of_bat = int(input("What is the angle of the bat: "))
+    speed_of_bat = int(input("What is speed of the bat in newtons: "))
+    speed_of_outfielder = 8.8
+    overall_speed_of_ball = speed_of_bat - speed_of_pitch
+    while angle_of_bat >= 0:
+        print "hit in play"
+
+
