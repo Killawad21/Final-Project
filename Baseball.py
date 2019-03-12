e793d820d899746760ad201066fae025b749fc53
Logan Pennock <02logan.pennock@gulllakecs.org>
2019-03-12 12:18:46 -0400

    added while loop

--- Baseball.py
+++ Baseball.py
@@ -2,9 +2,8 @@
 # single, double, triple, HOMERUN!!!
 # Logan Pennock
 
-x = y
-a = b
-c = d
+
+
 for i in range(3):
     speed_of_pitch = int(input("What is the speed of the pitch: "))
     angle_of_bat = int(input("What is the angle of the bat: "))
@@ -12,6 +11,13 @@ for i in range(3):
     speed_of_outfielder = 8.8
     overall_speed_of_ball = speed_of_bat - speed_of_pitch
     while angle_of_bat >= 0:
-        print "hit in play"
-
-
+        print ("hit in play")
+        if angle_of_bat <= 5:
+            print ("your out")
+            break
+        elif angle_of_bat == (6,):
+            print ("you got a single")
+            break
+        else:
+            print("you hit high enough to get more than a single")
+    
